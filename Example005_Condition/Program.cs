d3da429660cba8767b092e690a6678a0e4474ee9
Yupatova Anna <yupatova@gmail.com>
2022-07-02 15:14:09 +0500

    add Example_005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользоватея");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+Console.WriteLine("Привет, ");
+Console.Write(username);
+}
