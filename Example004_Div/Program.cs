9667e0eeb28f5e380b205b11ed206d88c789b903
Yupatova Anna <yupatova@gmail.com>
2022-07-02 15:00:46 +0500

    add example04_Div and Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA / numberB);
